24799ed9ea92a5f9a330cf631fd4295714cf23bc
theo-be <81959832+theo-be@users.noreply.github.com>
2024-04-03 13:16:02 +0200

    Correction panier

--- php/varSession.inc.php
+++ php/varSession.inc.php
@@ -3,8 +3,9 @@
 if (!isset($_SESSION["init"])) {
     $_SESSION["init"] = true;
     $_SESSION["tabcategories"] = null;
-    $_SESSION["panier"] = null;
+    $_SESSION["panier"] = [];
     $_SESSION["contact_ok"] = true;
+    $_SESSION["compte"] = false;
 
     // recuperation de la base de donnees
 
@@ -12,8 +13,11 @@ if (!isset($_SESSION["init"])) {
     $_SESSION["bdd"] = json_decode($dbtext, true);
 
 }
-
-
+//
+//if ($_SESSION["compte"]) {
+//    echo "bonjour, " . $_SESSION["id"] . "<br>";
+//} else echo "pas connecte<br>";
+//
 
 
 
