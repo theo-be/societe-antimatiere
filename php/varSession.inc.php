7d0db91590fd601e3f5443d14b19630e375bef4c
theo-be <81959832+theo-be@users.noreply.github.com>
2024-04-10 15:16:44 +0200

    Changement de la gestion de la base de données

--- php/varSession.inc.php
+++ php/varSession.inc.php
@@ -1,25 +1,23 @@
 <?php
 
+
+
+
+if (!isset($_SESSION)) {
+    session_start();
+    echo "pas de session";
+}
+
 if (!isset($_SESSION["init"])) {
+    echo "session pas init";
     $_SESSION["init"] = true;
     $_SESSION["tabcategories"] = null;
     $_SESSION["panier"] = [];
     $_SESSION["contact_ok"] = true;
     $_SESSION["compte"] = false;
-
-    // recuperation de la base de donnees
-
-    $dbtext = file_get_contents("php/db.json");
-    $_SESSION["bdd"] = json_decode($dbtext, true);
-
+    $_SESSION["id"] = null;
+    
 }
-//
-//if ($_SESSION["compte"]) {
-//    echo "bonjour, " . $_SESSION["id"] . "<br>";
-//} else echo "pas connecte<br>";
-//
-
-
 
 ?>
 
