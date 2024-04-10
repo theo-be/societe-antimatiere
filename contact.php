dce68e31384f12c8c837e9a012795b713cb35e7c
Jagadai <106830744+Jagadai@users.noreply.github.com>
2024-04-10 15:19:50 +0200

    test

--- contact.php
+++ contact.php
@@ -14,7 +14,6 @@ require_once "php/varSession.inc.php";
     <link rel="stylesheet" href="css/styles.css">
     <meta name="description" content="">
     <link rel="icon" href="favicon.ico" type="image/x-icon">
-
 </head>
 
 
@@ -23,18 +22,21 @@ require_once "php/varSession.inc.php";
 <body>
 <header role="banner">
     <div class="banner">
-        <img src="icon.png" alt="Logo du site" id="logo">
+        <!-- Logo du site -->
+        <div class="bandeaubleu"></div>
+        <img src="icon.png" alt="Logo du site" width="300" height="150" id="logo">
         <h1 id="title">Société Antimatiere</h1>
+        <div class="bandeauvert"></div>
     </div>
     <nav>
-        <ul>
-            <li><a href="#">Accueil</a></li>
-            <li><a href="#">Générateurs d'Antimatière</a></li>
-            <li><a href="#">Améliorations Dimensionnelles</a></li>
-            <li><a href="#">Dispositifs de Manipulation Temporelle</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-    </nav>
+        <!-- Menu horizontal -->
+        <table class='menu'>
+            <tr href="#" class="side"><td><a class="sideecrit" >Accueil</a></td></tr>
+            <tr href="#" class="side"><td><a class="sideecrit" >Générateurs d'Antimatière</a></td></tr>
+            <tr href="#" class="side"><td><a class="sideecrit" >Améliorations Dimensionnelles</a></td></tr>
+            <tr href="#" class="side"><td><a class="sideecrit" >Dispositifs de Manipulation Temporelle</a></td></tr>
+            <tr href="#" class="side"><td><a class="sideecrit" >> Contact</a></td></tr>
+        </table>
     <div class="connexion">
 
         <a href="#">Se connecter</a>
@@ -62,6 +64,7 @@ require_once "php/varSession.inc.php";
         require_once "php/formulaire_contact_prerempli.php";
 //        echo "contact_ok : " . $_SESSION["contact_ok"] . "<br>";
     }
+//    var_dump($_SESSION["formulaire_contact"]);
 ?>
 <!--<script src="js/app.js"></script>-->
 
