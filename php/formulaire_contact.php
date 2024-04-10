76082810fcd8210c3ea82a2f0c8dd65fa71674db
Jagadai <106830744+Jagadai@users.noreply.github.com>
2024-04-10 19:14:56 +0200

    Add files via upload

--- php/formulaire_contact.php
+++ php/formulaire_contact.php
@@ -1,24 +1,32 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="stylesheet" href="accueil.css">
+    <meta name="description" content="">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+</head>
 
 <div class="contact">
     <form method="post" action="php/demandecontact.php">
         <h2>Demande de contact</h2>
         <!--<label>Date du contact</label><input type="date" name="date_contact" required><br><br>-->
-        <label>Nom</label> <input type="text" placeholder="Entrez Nom" name="nom" required><br><br>
-        <label>Prenom</label> <input type="text" placeholder="Entrez Prenom" name="prenom" required><br><br>
-        <label>Email</label> <input type="email" placeholder="monmail@monsite.org" name="email" required><br><br>
-        <label>Genre</label><input type="radio" id="femme" name="genre" value="Femme"><label for="femme">Femme</label>
-        <input type="radio" id="homme" name="genre" value="Homme"><label for="homme">Homme</label><br><br>
-        <label>Date de Naissance</label><input type="date" name="date_naissance" required><br><br>
+        <label>Nom</label> <input class="formulaire" type="text" placeholder="Entrez Nom" name="nom" required><br><br>
+        <label>Prenom</label> <input class="formulaire" type="text" placeholder="Entrez Prenom" name="prenom" required><br><br>
+        <label>Email</label> <input class="formulaire" type="email" placeholder="monmail@monsite.org" name="email" required><br><br>
+        <label>Genre</label><input class="formulaire" type="radio" id="femme" name="genre" value="Femme"><label for="femme">Femme</label>
+        <input class="formulaire" type="radio" id="homme" name="genre" value="Homme"><label for="homme">Homme</label><br><br>
+        <label>Date de Naissance</label><input class="formulaire" type="date" name="date_naissance" required><br><br>
         <label for="fonction">Fonction</label>
-        <select name="fonction" id="fonction">
+        <select class="formulaire" name="fonction" id="fonction">
             <option value="Ens">Enseignant</option>
             <option value="Eleve">Eleve</option>
             <option value="Autre">Autre</option>
         </select>
         <br><br>
-        <label>Sujet:</label><input type="text" placeholder="Entrez le sujet de votre mail" name="sujet" required><br><br>
-        <label>Contenu:</label><input type="text" placeholder="Tapez ici votre mail" name="contenu" required>   <br><br>
-        <button type="submit">Envoyer</button>
+        <label>Sujet</label><input class="formulaire" type="text" placeholder="Entrez le sujet de votre mail" name="sujet" required><br><br>
+        <label>Contenu</label><input class="formulaire" type="text" placeholder="Tapez ici votre mail" name="contenu" required>   <br><br>
+        <button class="submit" type="submit">Envoyer</button>
     </form>
 
 </div>
