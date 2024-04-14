5561a9d8ad252de4f7e138e39ecf0123caafe01e
theo-be <81959832+theo-be@users.noreply.github.com>
2024-04-14 18:43:48 +0200

    Fix date de naissance dans la bdd

--- php/demandecontact.php
+++ php/demandecontact.php
@@ -7,22 +7,14 @@
 
     $_SESSION["contact_ok"] = true;
 
-    $form_content_keys = array("nom", "prenom", "email", "genre", "date_naissance", "fonction", "sujet", "contenu");
+    $clef_dans_formulaire = array("nom", "prenom", "email", "genre", "date_naissance", "fonction", "sujet", "contenu");
 
-    $diff = array_diff($form_content_keys, array_keys($_POST));
+    $diff = array_diff($clef_dans_formulaire, array_keys($_POST));
 
     // verifie que l'on a toutes les valeurs que l'on veut avoir, ni plus, ni moins
-    if (count($diff) > 0 && count(array_merge($form_content_keys, $diff)) <= count($form_content_keys))
+    if (count($diff) > 0 && count(array_merge($clef_dans_formulaire, $diff)) <= count($clef_dans_formulaire))
         $_SESSION["contact_ok"] = false;
 
-
-//    foreach ($_POST as $key => $element) {
-//        if (!in_array($key, $form_content_keys)) {
-//            $_SESSION["contact_ok"] = false;
-//            $_SESSION["formulaire_contact"][$key] = "";
-//
-//        }
-//    }
 
     $_SESSION["formulaire_contact"]["erreur"] = $diff;
 
@@ -37,14 +29,6 @@
 
     $_SESSION["formulaire_contact"] = array_merge($_SESSION["formulaire_contact"], $_POST);
     $_SESSION["formulaire_contact"]["date_contact"] = date("Y-m-d");
-/*    $_SESSION["formulaire_contact"]["nom"] = $_POST["nom"];
-    $_SESSION["formulaire_contact"]["prenom"] = $_POST["prenom"];
-    $_SESSION["formulaire_contact"]["email"] = $_POST["email"];
-    $_SESSION["formulaire_contact"]["genre"] = $_POST["genre"];
-    $_SESSION["formulaire_contact"]["date_naissance"] = $_POST["date_naissance"];
-    $_SESSION["formulaire_contact"]["fonction"] = $_POST["fonction"];
-    $_SESSION["formulaire_contact"]["sujet"] = $_POST["sujet"];
-    $_SESSION["formulaire_contact"]["contenu"] = $_POST["contenu"];*/
 
 
     // on convertit les caractetes speciaux s'il y en a pour eviter des failles de securite
@@ -58,7 +42,7 @@
 
 
 
-    if (!preg_match("#^([0-9a-z]){3,}@([a-z]){2,}.([a-z]){2,4}$#i", $_POST["email"])) {
+    if (!preg_match("#^([0-9a-z.]){3,}@([a-z]){2,}.([a-z]){2,4}$#i", $_POST["email"])) {
         // email invalide
         echo "email invalide<br>";
         $_SESSION["contact_ok"] = false;
@@ -71,35 +55,25 @@
         $_SESSION["contact_ok"] = false;
     }
 
-
-
-
-
     // si tout est bon, alors on envoie les donnees dans la bdd et on les libere de la session
 
     if ($_SESSION["contact_ok"] === true) {
         // preparation des scripts SQL
         // envoi vers la bdd
 
-        $texterequete = file_get_contents("sql/BDD inscription client.sql");
-//        $requete = $_SESSION["bdd"]->prepare("insert into clients (nom, prenom, email, genre, metier, date_de_naissance, date_de_contact) values (?, ?, ?, ?, ?, STR_TO_DATE(?, 'YYYY-MM-DD'), NOW())");
-
         $db = new PDO('mysql:host=localhost;dbname=antimaterDimension', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-        $requete = $db->prepare("insert into clients (nom, prenom, email, genre, metier, date_de_naissance, date_de_contact) values (?, ?, ?, ?, ?, STR_TO_DATE(?, 'YYYY-MM-DD'), NOW())");
+        $requete = $db->prepare("insert into clients (nom, prenom, email, genre, metier, date_de_naissance) values (?, ?, ?, ?, ?, STR_TO_DATE(?, '%Y-%m-%d'))");
         $requete->execute(array(
             $_SESSION["formulaire_contact"]["nom"],
             $_SESSION["formulaire_contact"]["prenom"],
             $_SESSION["formulaire_contact"]["email"],
             $_SESSION["formulaire_contact"]["genre"],
             $_SESSION["formulaire_contact"]["fonction"],
-            $_SESSION["formulaire_contact"]["date_de_naissance"],
+            $_SESSION["formulaire_contact"]["date_naissance"],
         ));
 
         $requete->closeCursor();
 
-
-
-// code_client 	nom 	prenom 	email 	genre 	metier 	date_de_naissance 	date_de_contact
         $_SESSION["formulaire_contact"] = null;
         header("Location:../contact.php");
     }
