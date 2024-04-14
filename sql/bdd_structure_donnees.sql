efd6f242d8ac8a5d061a5991c5a9a353f380e550
theo-be <81959832+theo-be@users.noreply.github.com>
2024-04-14 19:27:01 +0200

    Fix date

--- sql/bdd_structure_donnees.sql
+++ sql/bdd_structure_donnees.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 10, 2024 at 04:26 PM
+-- Generation Time: Apr 14, 2024 at 07:25 PM
 -- Server version: 10.6.16-MariaDB-0ubuntu0.22.04.1
 -- PHP Version: 8.1.2-1ubuntu2.14
 
@@ -40,8 +40,8 @@ CREATE TABLE `categorie` (
 
 INSERT INTO `categorie` (`id`, `nom`) VALUES
 (1, 'Celestial'),
-(2, 'Glyph'),
-(3, 'Succes');
+(2, 'Glyphe'),
+(3, 'Sticker');
 
 -- --------------------------------------------------------
 
@@ -57,7 +57,7 @@ CREATE TABLE `clients` (
   `genre` varchar(6) DEFAULT NULL,
   `metier` varchar(30) DEFAULT NULL,
   `date_de_naissance` date DEFAULT NULL,
-  `date_de_contact` date DEFAULT NULL
+  `date_de_contact` date DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -65,15 +65,14 @@ CREATE TABLE `clients` (
 --
 
 INSERT INTO `clients` (`code_client`, `nom`, `prenom`, `email`, `genre`, `metier`, `date_de_naissance`, `date_de_contact`) VALUES
-(1, 'a', 'a', 'abcd@abcd.bde', 'Femme', NULL, NULL, NULL),
-(2, 'a', 'a', 'abcd@abcd.bde', 'Femme', NULL, NULL, NULL),
-(3, 'a', 'a', 'abcd@abcd.bde', 'Femme', NULL, NULL, NULL),
-(4, 'a', 'a', 'abcd@abcd.bde', 'Femme', 'Ens', NULL, NULL),
 (5, 'a', 'a', 'abcd@abcd.bde', 'Femme', 'Ens', NULL, '2024-04-09'),
 (6, 'a', 'a', 'abcd@abcd.bde', 'Femme', 'Ens', NULL, '2024-04-09'),
 (7, 'a', 'a', 'abcd@abcd.bde', 'Femme', 'Ens', NULL, '2024-04-09'),
 (8, 'a', 'a', 'abcd@abcd.bde', 'Femme', 'Ens', NULL, '2024-04-09'),
-(9, 'a', 'a', 'abcd@abcd.bde', 'Femme', 'Ens', NULL, '2024-04-10');
+(9, 'a', 'a', 'abcd@abcd.bde', 'Femme', 'Ens', NULL, '2024-04-10'),
+(10, 'moimoimoi', 'c\'est moi', 'moi@moi.moi', 'Homme', 'Ens', NULL, '2024-04-12'),
+(11, 'a', 'a', 'abcd@abcd.bde', 'Homme', 'Autre', NULL, '2024-04-14'),
+(12, 'b', 'b', 'belliere.theo@outlook.fr', 'Homme', 'Eleve', '2003-05-05', '2024-04-14');
 
 -- --------------------------------------------------------
 
@@ -96,7 +95,7 @@ CREATE TABLE `commande` (
 CREATE TABLE `compte` (
   `id` int(11) NOT NULL,
   `pseudo` varchar(30) DEFAULT NULL,
-  `mdp` varchar(100) DEFAULT NULL,
+  `mdp` varchar(60) DEFAULT NULL,
   `code_client` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -110,7 +109,9 @@ INSERT INTO `compte` (`id`, `pseudo`, `mdp`, `code_client`) VALUES
 (3, 'b', '$2y$10$RbHOrzjhynWAhh9XFmuazOPOoj/k8ldalSeqsrLaiNZMZRaLzQMYS', NULL),
 (4, 'c', '$2y$10$U3bPq.lMhvV9/MuiSPCj9e7A4HhCgkGgzoSst383TDsav2IJkPmeG', NULL),
 (5, 'd', '$2y$10$JJ1LswnrsDVVabLyxE2VYehgkG3uACFfc0/HQmyxvDBU5MewNurmq', NULL),
-(6, 'e', '$2y$10$yIGNJh18I.8ok9C6JdUSlufr4kjPGOsW1yROEtzMI5dwaX3SyAvrm', NULL);
+(6, 'e', '$2y$10$yIGNJh18I.8ok9C6JdUSlufr4kjPGOsW1yROEtzMI5dwaX3SyAvrm', NULL),
+(7, 'z', '$2y$10$e/lkI2YxCpy4yyb/EHmsO.NUKF6BVsXHm3YwcEE903.YnaHxvPIs2', NULL),
+(8, '</div>', '$2y$10$.wFTNBnr2Vq2AToYhZz0zuz2zaLVmslYQ9HjRTnikG/ywr.VrqmEy', NULL);
 
 -- --------------------------------------------------------
 
@@ -133,19 +134,19 @@ CREATE TABLE `produit` (
 --
 
 INSERT INTO `produit` (`id`, `nom`, `quantite_en_stock`, `id_categorie`, `photo`, `text_description`, `prix`) VALUES
-(1, 'Teresa', 10, 1, 'cel_teresa.png', 'Teresa, celestial des realités', 1),
-(2, 'Effarig', 10, 1, 'cel_effarig.png', 'Effarig, celestial des anciennes reliques', 1),
-(3, 'Anonyme', 10, 1, 'cel_anonyme.png', 'L\'anonyme, celestial des prisoniers', 1),
-(4, 'V', 10, 1, 'cel_v.png', 'V, celestial des succes', 1),
-(5, 'Ra', 10, 1, 'cel_ra.png', 'Ra, celestial de l\'oublie', 1),
-(6, 'Lai\'tela', 10, 1, 'cel_laitela.png', 'Lai\'tela, celestial de l\'antimatiere', 1),
-(7, 'Glyphe de puissance', 10, 2, 'glyphe_puissance.png', 'Amplifie la puissance des dimensions d\'antimatiere', 1),
-(8, 'Glyphe de l\'infini', 10, 2, 'glyphe_infinite.png', 'Amplifie la puissance des dimensions d\'infinité', 1),
+(1, 'Teresa', 10, 1, 'cel_teresa.png', 'Teresa, célestial de la realité', 1),
+(2, 'Effarig', 10, 1, 'cel_effarig.png', 'Effarig, célestial des anciennes reliques', 1),
+(3, 'L\'Anonyme', 10, 1, 'cel_anonyme.png', 'L\'anonyme, célestial des prisonniers', 1),
+(4, 'V', 10, 1, 'cel_v.png', 'V, célestial des succès', 1),
+(5, 'Ra', 10, 1, 'cel_ra.png', 'Ra, célestial de l\'oublié', 1),
+(6, 'Lai\'tela', 10, 1, 'cel_laitela.png', 'Lai\'tela, célestial de l\'antimatière', 1),
+(7, 'Glyphe de puissance', 10, 2, 'glyphe_puissance.png', 'Amplifie la puissance des dimensions d\'antimatière', 1),
+(8, 'Glyphe d\'infinité', 10, 2, 'glyphe_infinite.png', 'Amplifie la puissance des dimensions d\'infinité', 1),
 (9, 'Glyphe de temps', 10, 2, 'glyphe_temps.png', 'Amplifie la puissance des dimensions temporelles', 1),
-(10, 'Glyphe de replication', 10, 2, 'glyphe_replication.png', 'Amplification des reproduction des replicantis', 1),
-(11, 'Glyphe de dilatation', 10, 2, 'glyphe_dilatation.png', 'Amplifie la puissance des dimensions d\'antimatiere lors de la dilatation', 1),
+(10, 'Glyphe de réplication', 10, 2, 'glyphe_replication.png', 'Amplifie l\'efficacité de la reproduction des réplicantis', 1),
+(11, 'Glyphe de dilatation', 10, 2, 'glyphe_dilatation.png', 'Amplifie la puissance des dimensions d\'antimatière lors de la dilatation du temps', 1),
 (12, 'Glyphe d\'Effarig', 10, 2, 'glyphe_effarig.png', 'Amplifie les effets des reliques', 1),
-(13, 'Glyphe de la realite', 10, 2, 'glyphe_realite.png', 'Amplifie les bonus de la réalité', 1),
+(13, 'Glyphe de la réalité', 10, 2, 'glyphe_realite.png', 'Amplifie les bonus de la réalité', 1),
 (14, 'Fake News', 10, 3, 'succes_news.png', 'Description', 1),
 (15, 'Eternité', 10, 3, 'succes_eternite.png', 'Description', 1),
 (16, 'Ticket McDo', 10, 3, 'succes_mcdo.png', 'Description', 1),
@@ -199,13 +200,13 @@ ALTER TABLE `produit`
 -- AUTO_INCREMENT for table `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `code_client` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `code_client` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `compte`
 --
 ALTER TABLE `compte`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
