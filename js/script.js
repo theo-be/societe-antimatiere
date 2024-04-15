1dff1a02e7e50364fe8d40cabc7d24d8a4e5ba03
Pauline-Mac <104761610+Pauline-Mac@users.noreply.github.com>
2024-04-15 22:32:05 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -30,22 +30,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 function show_stock() {
-    // Get the input element with the ID "quantite"
     var inputStock = document.getElementById("stock");
     var stockButton = document.querySelector(".stock-btn");
 
-    // Check if the input element exists
+
     if (inputStock && stockButton) {
-        // Check the current type of the input field
+
         if (inputStock.type === "text") {
-            // If it's currently text, change it to hidden
             inputStock.type = "hidden";
-            // Change the button text to "Voir le stock"
+
             stockButton.textContent = "Voir le stock";
         } else {
-            // If it's not text (e.g., hidden), change it to text
+
             inputStock.type = "text";
-            // Change the button text to "Masquer le stock"
+
             stockButton.textContent = "Masquer le stock";
         }
     }
