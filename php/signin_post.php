f5b90dfe30d2b4e9faf59103f7f552efe76d0d03
Pauline-Mac <104761610+Pauline-Mac@users.noreply.github.com>
2024-04-15 22:27:39 +0200

    Add files via upload

--- php/signin_post.php
+++ php/signin_post.php
@@ -1,13 +1,17 @@
 <?php
-
 session_start();
 require_once "varSession.inc.php";
 require_once "bdd.php";
 
+$_SESSION['errors'] = array();
+
+
 if (!isset($_POST["id"]) || !isset($_POST["mdp"]) || !isset($_POST["mdp2"])){
-    // formulaire incomplet
+    $_SESSION['errors'][] = "Formulaire incomplet";
     header("Location:inscription.php");
+    exit();
 }
+
 
 $id = $_POST["id"];
 $mdp = $_POST["mdp"];
@@ -15,30 +19,22 @@ $mdp2 = $_POST["mdp2"];
 
 
 
+
 if ($mdp != $mdp2) {
-    // les mdp correspondent pas
+    $_SESSION['errors'][] = "Les mots de passe ne correspondent pas.";
     header("Location:inscription.php");
-} else if (strlen($mdp) < 8) {
-    // nombre minimal de caracteres non respecte
+    exit();
+} elseif (strlen($mdp) < 8) {
+    $_SESSION['errors'][] = "Le mot de passe doit contenir au moins 8 caractères.";
     header("Location:inscription.php");
-} else
-// insertion dans la bdd
-
-if (inscription($id, $mdp)) {
-    header("Location:" . $_SESSION["page"]);
-
-    $_SESSION["compte"] = true;
-    $_SESSION["id"] = htmlspecialchars($id);
-
-    echo $_SESSION["id"];
-}
-else {
+    exit();
+} elseif (!inscription($id, $mdp)) {
+    $_SESSION['errors'][] = "Une erreur s'est produite lors de l'inscription. Veuillez réessayer.";
     header("Location:inscription.php");
+    exit();
 }
 
-// si retour faux alors le compte existe deja
-
-
-
-
+$_SESSION["compte"] = true;
+$_SESSION["id"] = htmlspecialchars($id);
+header("Location:" . $_SESSION["page"]);
 ?>
