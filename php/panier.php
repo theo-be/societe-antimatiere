5baff5667fa7ab8782f862ceececccb20562e049
theo-be <81959832+theo-be@users.noreply.github.com>
2024-04-18 18:49:05 +0200

    Verification des stocks

--- php/panier.php
+++ php/panier.php
@@ -4,21 +4,33 @@
 session_start();
 require_once "varSession.inc.php";
 
+
 $idproduit = $_POST["id"];
 $quantite = $_POST["quantite"];
 $danspanier = false;
 
-for ($i = 0; $i < count($_SESSION["panier"]); $i++) {
-    if ($_SESSION["panier"][$i]["id"] == $idproduit) {
-        $_SESSION["panier"][$i]["quantite"] = $quantite;
-        $danspanier = true;
-        break;
+
+// verification de l'existance du produit et des stocks
+
+$db = new PDO('mysql:host=localhost;dbname=antimaterDimension', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+$requete = $db->prepare("SELECT quantite_en_stock FROM produit WHERE id=?");
+$requete->execute(array($idproduit));
+
+if (($produit = $requete->fetch())) {
+    for ($i = 0; $i < count($_SESSION["panier"]); $i++) {
+        if ($_SESSION["panier"][$i]["id"] == $idproduit) {
+            $_SESSION["panier"][$i]["quantite"] = min($quantite, $produit["quantite_en_stock"]);
+            $danspanier = true;
+            break;
+        }
+    }
+
+    if (!$danspanier) {
+        $_SESSION["panier"][] = ["id" => $idproduit, "quantite" => min($quantite, $produit["quantite_en_stock"])];
     }
 }
 
-if (!$danspanier) {
-    $_SESSION["panier"][] = ["id" => $idproduit, "quantite" => $quantite];
-}
+$requete->closeCursor();
 
 header("Location:". $_SESSION["page"]);
 
