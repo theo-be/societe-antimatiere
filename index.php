67e2a676ae06164a150d5f279ed3a98d265eb542
Pauline-Mac <104761610+Pauline-Mac@users.noreply.github.com>
2024-04-18 22:46:48 +0200

    CSS index et valider_commande séparer

--- index.php
+++ index.php
@@ -13,78 +13,12 @@ require_once "php/varSession.inc.php";
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap">
+    <link rel="stylesheet" href="css/index.css">
     <link rel="icon" href="img/favicon.ico" type="image/x-icon">
     <title>societe antimatiere</title>
 </head>
 
-<style>
-    body{
-        overflow: hidden;
-        font-family: 'Inter', sans-serif;
-        background-image: url('img/stars-bg.png');
-        background-size: cover;
-        background-position: center;
-        background-attachment: fixed;
-        margin: 0;
-        padding: 0;
-        text-align: center;
-        color: white;
-    }
 
-
-
-    .logo-box {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: fixed;
-        top: 10%;
-        left: 0;
-        right: 0;
-        width: 100%;
-        font-size: 30px;
-    }
-
-    .index-box{
-        top : 50vh;
-        margin: 0 auto;
-        position: relative;
-
-    }
-
-    #Progress_Status {
-        display: block;
-        margin: 0 auto;
-        width: 50%;
-        background-color: #000;
-    }
-
-    #myprogressBar {
-        width: 1%;
-        height: 35px;
-        background-color: green;
-        text-align: center;
-        line-height: 32px;
-        color: white;
-    }
-
-    .submit{
-        margin: 20px;
-        font-size: large;
-        border: 1px solid green;
-        background-color: black;
-        color:white;
-        padding:10px;
-        transition:200ms;
-    }
-
-    .submit:hover{
-        box-shadow: 0 0 20px 10px darkgreen;
-        background-color: green;
-        color:black;
-    }
-
-</style>
 <body>
 
 <div class="logo-box">
